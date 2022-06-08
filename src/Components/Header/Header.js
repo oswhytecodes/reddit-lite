94cf0ba3cc85ec3549abf823fc536d5c43fba504
Orincy Whyte <orincywhyte@gmail.com>
2022-06-08 15:24:31 -0400

    added Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../assets/images/logo.png'
+
+const Header = () => {
+
+    return (
+        <div className='logo'>
+            <img src={logo} alt="logo" />
+        </div>
+    )
+}
+export default Header
