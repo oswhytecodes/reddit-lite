2ccbcaca78e296505a05781a182486ef60ffc006
oswhyteknits <orincywhyte@gmail.com>
2022-06-12 11:57:18 -0400

    removed inline styles

--- src/Components/PostContainer/PostCards.js
+++ src/Components/PostContainer/PostCards.js
@@ -33,31 +33,13 @@ export const PostCards = () => {
               className="row two"
             >
               <div style={{ display: "flex" }} className="column one">
-                <div
-                  className="upvotes"
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: "8px",
-                    fontSize: "1.2em",
-                  }}
-                >
+                <div className="upvotes">
                   <i className="fa-solid fa-arrow-up"></i>
                   <p style={{ color: "#333" }}>{x.data.ups}</p>
                   <i className="fa-solid fa-arrow-down"></i>
                 </div>
 
-                <div
-                  className="column two"
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    marginLeft: "3.5em",
-                    gap: "1em",
-                  }}
-                >
+                <div className="column-two">
                   <p style={{ fontSize: "1.3rem" }}>{x.data.title}</p>
 
                   <img
@@ -68,9 +50,7 @@ export const PostCards = () => {
                   />
                   <div style={styles} className="row four">
                     <p>{x.data.author}</p>
-                    <div
-                      style={{ display: "flex", gap: "5px", padding: "0 5px" }}
-                    >
+                    <div className="comments">
                       <i className="fa-solid fa-comment"></i>
                       <p>{x.data.num_comments}</p>
                     </div>
