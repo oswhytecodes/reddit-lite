59caa75acbcf6547af49f69d1d937534077eb7b9
oswhyteknits <orincywhyte@gmail.com>
2022-06-13 13:37:11 -0400

    hard coded subreddit data for styling

--- src/Components/SubredditContainer/SubredditCard.js
+++ src/Components/SubredditContainer/SubredditCard.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export const SubredditCard = () => {
+  const categories = [
+    "memes",
+    "Futurology",
+    "learnjavascript",
+    "learnprogramming",
+    "webdev",
+    "reactjs",
+    "tailwindcss",
+    "Frontend",
+  ];
   return (
-    <div className='SubredditCard'>SubredditCard
-      <div>
-        
-      </div>
+    <div className="SubredditCard">
+      {categories.map((category, index) => {
+        return (
+          <li key={index} className="category-card">
+            r/{category}
+          </li>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
--- src/Redux/RedditSlice.js
+++ src/Redux/RedditSlice.js
@@ -1,20 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from 'axios'
 
 const initialState = {
   data: [],
   loading: false,
   error: "",
-  categories: "",
+  // category: "memes",
 };
 
-//  you have to make two calls
-//  first call is for the home page, and the other is to link categories
 export const fetchData = createAsyncThunk("reddit/fetchData", async () => {
   // const url = `https://www.reddit.com/r/${category}.json`
-  const data = await fetch(`https://www.reddit.com/r/memes.json`);
+  
+  const data = await fetch(`https://www.reddit.com/r/happy.json`);
   const json = await data.json();
-  const fetchedData = json.data.children
+  const fetchedData = json.data.children;
   return fetchedData;
 });
 
