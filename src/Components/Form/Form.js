75041930899b02b21b2ece02c5e1ce4ba953a6d6
oswhyteknits <orincywhyte@gmail.com>
2022-06-14 06:32:37 -0400

    form submit initialize

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,14 +1,27 @@
- const Form = () =>{
-    return(
-        <div className="Form">
-            <input 
-            className="input-form"
-            type="text" 
-            placeholder="Search topic..."
-            // value={value}
-            onChange={(e) => console.log(e.target.value)}
-            />
-        </div>
-    )
-}
-export default Form
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchData } from "../../Redux/RedditSlice";
+const Form = () => {
+  const [value, setValue] = useState("");
+  const filteredSubReddit = useSelector((state) => state.reddit.data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValue("");
+    console.log(value);
+  };
+  return (
+    <div className="Form">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input-form"
+          type="text"
+          placeholder="Search topic..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+};
+export default Form;
